6ca697aee442fe1d7d5310ae4ab5ec9254d7e0e6
Akashdeep Singh <bhanguakash27@gmail.com>
2024-08-08 18:05:36 -0700

    merged with sonali's styling and pujan's changes

--- src/Components/StudentAuthPage.js
+++ src/Components/StudentAuthPage.js
@@ -162,77 +162,20 @@ const StudentAuthPage = ({ apiUrl }) => {
         <button className="toggle-button" onClick={toggleAuthMode}>
           {isLogin ? "Don't have an account? Sign Up" : 'Already have an account? Login'}
         </button>
-      </div>
-    <div className="auth-container">
-      <h1>{isLogin ? 'Student Login' : 'Student Sign Up'}</h1>
-      <form onSubmit={handleAuth}>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+        {showError && (
+          <CustomErrorMessage
+            message={error.split('\n').map((msg, idx) => (
+              <React.Fragment key={idx}>
+                {msg}
+                <br />
+              </React.Fragment>
+            ))}
+            onClose={closeError}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">{isLogin ? 'Password:' : 'Create Password:'}</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {!isLogin && (
-          <>
-            <div className="form-group">
-              <label htmlFor="fullName">Full Name:</label>
-              <input
-                type="text"
-                id="fullName"
-                name="fullName"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="studentNumber">Student Number:</label>
-              <input
-                type="text"
-                id="studentNumber"
-                name="studentNumber"
-                value={studentNumber}
-                onChange={(e) => setStudentNumber(e.target.value)}
-                required
-              />
-            </div>
-          </>
         )}
-        <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
-      </form>
-      <button className="toggle-button" onClick={toggleAuthMode}>
-        {isLogin ? "Don't have an account? Sign Up" : 'Already have an account? Login'}
-      </button>
-      {showError && (
-        <CustomErrorMessage
-          message={error.split('\n').map((msg, idx) => (
-            <React.Fragment key={idx}>
-              {msg}
-              <br />
-            </React.Fragment>
-          ))}
-          onClose={closeError}
-        />
-      )}
+      </div>
     </div>
   );
-  </div> )
 };
 
 export default StudentAuthPage;
